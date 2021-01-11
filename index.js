fd1b4885ce3880b5b073b0c621f710616241f9da
zhangyunlong.zhang <zhangyunlong.zhang@qunar.com>
2021-01-11 16:56:51 +0800

    source

--- index.js
+++ index.js
@@ -21,3 +21,68 @@ function is(x,y){
         return x !== y && y !== x;
     }
 }
+
+
+function findIndex(arr,target){
+    const len = arr.length;
+    let left = 0;
+    let right = len - 1;
+    let ret = -1;
+    while(left <= right){
+        const mid = ((right - left) >> 1) + left;
+        const val = arr[mid];
+        if(val >= target){
+            if(val === target){
+                ret = mid;
+                break;
+            }
+            right  = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+
+    return ret;
+}
+
+// console.log(findIndex([1,2,3,4,5,6],6))
+const removeDuplicate = (arr) => {
+    arr.sort();
+    let len = 1;
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] !== arr[i - 1]) arr[len++] = arr[i]
+    }
+    console.log(arr);
+    console.log(len);
+    arr.splice(len);
+    return arr;
+}
+
+// console.log(removeDuplicate([1,2,3]))
+
+
+const permute = (nums) => {
+    const res = [];
+    const used = {};
+  
+    function dfs(path) {
+        debugger;
+      if (path.length == nums.length) {
+        res.push(path.slice());
+        return;
+      }
+      for (const num of nums) {
+        // if (path.includes(num)) continue; // 查找的时间是O(n)，别这么写，时间复杂度增加
+        if (used[num]) continue;
+        path.push(num);
+        used[num] = true;
+        dfs(path);
+        path.pop();
+        used[num] = false;
+      }
+    }
+  
+    dfs([]);
+    return res;
+  };
+console.log(permute([1,2,3]));
--- js/n_sum.js
+++ js/n_sum.js
@@ -1,40 +1,33 @@
-// n数之和
-// 常见解法 双指针 回溯
-// 参考 https://github.com/sisterAn/JavaScript-Algorithms/issues/128
+function numSum(nums, n, m) {
+    if (!nums.length || nums.length < n) return [];
+    nums = nums.sort((a, b) => a - b);
+    const result = [];
+    const stack = [];
 
-/**
- * 
- * @param {*} arr 
- * @param {*} count 
- * @param {*} sum 
- */
-const search = (arr,count,sum) => {
-    const getCount = num => {
-        let count = 0;
-        while(num){
-            num &= (num - 1);
-            count ++;
-        }
-        return count;
-    }
+    const backtrace = (start) => {
+        if (stack.length === n - 1) {
+            let end = nums.length - 1;
 
-    let len = arr.length;
-    let bit = 1 << len;
-    let res = [];
-    for(let i = 0; i < bit; i++){
-        if(getCount(i) === count){
-            let s = 0;
-            let temp = [];
-            for(let j = 0; j < len; j++){
-                if(i & 1 << (len - 1 - j)){
-                    s += arr[j];
-                    temp.push(arr[j]);
+            while (start <= end) {
+                const temp = stack.reduce((acc, cur) => acc + cur);
+                if (temp + nums[start] === m) {
+                    result.push([...stack, nums[start]]);
                 }
+                if (start !== end && temp + nums[end] === m) {
+                    result.push([...stack, nums[end]]);
+                }
+                start++;
+                end--;
             }
-            if(s === sum){
-                res.push(temp);
-            }
+            return;
         }
-    }
-    return res;
+
+        for (let i = start; i < nums.length; i++) {
+            stack.push(nums[i]);
+            backtrace(i + 1);
+            stack.pop();
+        }
+    };
+    backtrace(0);
+    return result;
 }
