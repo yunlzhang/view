92e700ce40f95cd4999b94d12a2b3b4b7253a871
zhangyunlong.zhang <zhangyunlong.zhang@qunar.com>
2023-03-24 17:32:10 +0800

    update

--- js/kmp.js
+++ js/kmp.js
@@ -13,6 +13,8 @@ function kmp(s,t){
         next[i] = j;
     }
 
+    console.log(next);
+
     for(let i = 0, j = -1; i < sLen; i++){
         while(j !== -1 && s[i] !== t[j + 1]){
             j = next[j];
@@ -27,43 +29,6 @@ function kmp(s,t){
     return false;
 }
 
-function kmp2(s, t){
-    let sLen = s.length;
-    let tLen = t.length;
+console.log(kmp('aaaabaab', 'aabaa'));
 
-    let next = getNext(t);
-    let i = 0; let j = -1;
-
-    while(i < sLen && j < tLen - 1){
-        if(s[i] === t[j + 1]){
-            i++;
-            j++;
-        }else if(j === -1){
-            i++;
-        }else{
-            j = next[j];
-        }
-    }
-    // console.log(i - j - 1);
-    return j === tLen - 1 ? true : false;
-
-    function getNext(t){
-        let tLen = t.length;
-        let next = [-1];
-        let i = 1, j = -1;
-        // 前缀最大值减1;
-        while(i < tLen){
-            if(t[i] === t[j + 1]){
-                next[i++] = ++j;
-            }else if(j === -1){
-                next[i++] = -1;
-            }else{
-                j = next[j]
-            }
-        }
-        return next;
-    }
-}
-
-
-console.log(kmp('aaaabaab', 'ba'));
+debugger;
